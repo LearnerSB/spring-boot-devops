0d3e8e115a7e86f610c5397c5455d570337a2e7d
Shibaranjan Bhattacharyya <shibaranjan.bhattacharyya@cognizant.com>
2020-05-02 22:45:26 +0530

    initial Changes with distributionmanagement scm and plugins

---
+++ src/main/java/org/springtutorial/springbootdevops/SpringBootDevopsApplication.java
@@ -0,0 +1,13 @@
+package org.springtutorial.springbootdevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDevopsApplication.class, args);
+	}
+
+}
